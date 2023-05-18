9884d59e50fd6b11a74f5980229f0506403577b8
sunga <eert2260@naver.com>
2023-05-18 23:55:44 +0900

    feat: create project

---
+++ src/main/java/com/deepdive/puppypopularvote/PuppyPopularVoteApplication.java
@@ -0,0 +1,13 @@
+package com.deepdive.puppypopularvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyPopularVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyPopularVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepdive/puppypopularvote/PuppyPopularVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepdive.puppypopularvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyPopularVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
